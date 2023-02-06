2b0a9898f2a2837cf919ed0a8b1d49cf36b57f7e
Ultragum <89540611+Ultragum@users.noreply.github.com>
2023-02-05 22:57:23 -0500

    Fixed some minor issues and added the report

--- prog1-extractor/src/MainExtractor.java
+++ prog1-extractor/src/MainExtractor.java
@@ -6,20 +6,20 @@ public class MainExtractor {
     public static void main(String[] args) {
         SiteHandler site = new SiteHandler();
         
-        if (site.DownloadSite("https://www.cdc.gov/flu/avianflu/avian-in-humans.htm", "avian-flu-cdc.txt")) {
+        if (site.DownloadSite("https://www.cdc.gov/flu/avianflu/avian-in-humans.htm", "data/avian-flu-cdc.txt")) {
         	System.out.println("Information from cdc.gov successfully downloaded");
         }
         
-        if (site.DownloadSite("https://www.webmd.com/cold-and-flu/flu-guide/what-know-about-bird-flu", "avian-flu-webmd.txt")) {
+        if (site.DownloadSite("https://www.webmd.com/cold-and-flu/flu-guide/what-know-about-bird-flu", "data/avian-flu-webmd.txt")) {
         	System.out.println("Information from webmd.com successfully downloaded");
         	System.out.println();
         }
                 
-        if (site.FilterSite("avian-flu-cdc.txt", "cdc-filtered.txt")) {
+        if (site.FilterSite("data/avian-flu-cdc.txt", "data/cdc-filtered.txt")) {
         	System.out.println("Data from cdc.gov successfully filtered");
         }
                 
-        if (site.FilterSite("avian-flu-webmd.txt", "webmd-filtered.txt")) {
+        if (site.FilterSite("data/avian-flu-webmd.txt", "data/webmd-filtered.txt")) {
         	System.out.println("Data from webmd.com successfully filtered");
         }
     }
