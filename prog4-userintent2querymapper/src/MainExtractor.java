8bd5a09ca2270be651728a2c1ff6c2cb8533c829
Ultragum <89540611+Ultragum@users.noreply.github.com>
2023-03-26 21:52:47 -0400

    Added the fourth part of the project

---
+++ prog4-userintent2querymapper/src/MainExtractor.java
@@ -0,0 +1,81 @@
+/*
+ * Written by Jamel Chouarfia
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainExtractor {
+    public static void main(String[] args) {
+        ArrayList<String> childDirectories = new ArrayList<String>();
+        ArrayList<String> secondChildDirectories = new ArrayList<String>();
+        Scanner input = new Scanner(System.in);
+        String query = "";
+    	String temp = "";
+        int timer = 0;
+    	
+        // Downloads the main site for avian flu from the CDC
+        if (NewSiteHandler.downloadSite("https://www.cdc.gov/flu/avianflu/", "data/raw/avian-flu-cdc.txt")) {
+        	System.out.println("Information from cdc.gov successfully downloaded");
+        }
+        
+        // Extracts links from the main page for use in the next while loop
+        if (NewSiteHandler.extractLinks("data/raw/avian-flu-cdc.txt")) {
+        	System.out.println("The links have been extracted");
+            childDirectories = NewSiteHandler.getLinks("data/filtered/cdc-links.txt");
+    	}
+            	
+    	// Downloads the pages for directories that are a child of the main page
+    	while (!childDirectories.isEmpty()) {
+    		temp = childDirectories.remove(0);
+    		temp = temp.trim();
+    		
+    		if (temp.length() > 14) {
+        		NewSiteHandler.downloadSite("https://www.cdc.gov"+temp, "data/raw/avian-flu-cdc-"+temp.substring(14)+".txt");
+        		secondChildDirectories.add(temp.substring(14));
+        		timer++;
+    		}
+    	}
+    	
+        // Extracts the raw data from the main page into something that can be easily readable by humans
+        if (NewSiteHandler.filterSite("data/raw/avian-flu-cdc.txt", "data/filtered/cdc-filtered.txt")) {
+        	System.out.println("Data from cdc.gov successfully filtered");
+        }
+            
+        // Extracts the rest of the raw data into something that can be easily readable by humans
+        while (timer >= 0) {
+        	if (secondChildDirectories.isEmpty()) {
+        		break;
+        	}
+        	
+        	temp = secondChildDirectories.remove(0);
+        	temp = temp.trim();
+        	
+        	childDirectories.add(temp);
+        	
+        	NewSiteHandler.filterSite("data/raw/avian-flu-cdc-"+temp+".txt", "data/filtered/cdc-filtered-"+temp+".txt");
+        	timer--;
+        }
+        
+        if (args.length != 0) {
+            NewSiteHandler.printQuery(childDirectories, args[0]);
+        }
+        
+        
+        // Tries to answer the user's questions
+        while (true) {
+            System.out.println("Please feel free to ask any question. If you are done, type \"Quit\"");
+            
+            query = input.nextLine();
+            
+            if (query.equals("Quit") || query.equals("quit") || query.equals("q")) {
+            	System.out.println("Goodbye!");
+            	input.close();
+            	
+            	return;
+            }
+            
+            NewSiteHandler.printQuery(childDirectories, query);
+        }
+    }
+}
---
+++ prog4-userintent2querymapper/src/NewSiteHandler.java
@@ -0,0 +1,457 @@
+/*
+ * Written by Jamel Chouarfia
+ */
+
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class NewSiteHandler {
+	private static HttpURLConnection siteConnection;
+	private static URL siteLocation;
+	
+	/**
+	 * Attempts to download the specified website into a .txt file
+	 * @param destination The website's URL
+	 * @param file The file name where the downloaded data is stored
+	 * @return True if the site downloaded, false if it didn't
+	 */
+	public static boolean downloadSite(String destination, String file) {
+		try {
+			siteLocation = new URL(destination);
+			siteConnection = (HttpURLConnection) siteLocation.openConnection();
+			
+			if (siteConnection.getResponseCode() == 200) {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(siteConnection.getInputStream()));
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+				String line = reader.readLine();
+				
+				while (line != null) {
+					writer.write(line);
+					writer.newLine();
+					line = reader.readLine();
+				}
+				
+				writer.close();
+				reader.close();
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	/**
+	 * Extracts links and puts them into a file called "Links.txt"
+	 * @param file The file that links are beinge extracted from
+	 * @return A true if links were extracted, false if not
+	 */
+	public static boolean extractLinks(String file) {
+		try {
+			Scanner reader = new Scanner(new File(file));
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("data/filtered/cdc-links-unorganized.txt")));
+			String line = reader.next();
+			
+			while (reader.hasNextLine()) {
+				if (line.contains("a href")) {
+					line = filterLink(line);
+					
+					if (line.charAt(line.length() - 1) == ' ') {
+						line = line.substring(0, line.length() - 1);
+					}
+					
+					if (!line.equals("There was no link")) {
+						writer.write(line);
+						writer.newLine();
+					}
+				}
+				
+				line = reader.nextLine();
+			}
+			
+			writer.close();
+			
+			organizeLinks("data/filtered/cdc-links.txt");
+			
+			return true;
+		} catch (Exception e) {
+			System.out.println(e);
+			return false;
+		}
+	}
+	
+	/**
+	 * Returns an arraylist of every link that should have its page downloaded
+	 * @param file The file that contains the links
+	 * @return An arraylist containing every link
+	 */
+	public static ArrayList<String> getLinks(String file) {
+		ArrayList<String> validLinks = new ArrayList<String>();
+		
+		try {
+			Scanner reader = new Scanner(new File(file));
+			String line = reader.next();
+			
+			while (reader.hasNext()) {
+				validLinks.add(line);
+				line = reader.nextLine();
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+			System.exit(2);
+		}
+		
+		return validLinks;
+	}
+	
+	/**
+	 * Filters the downloaded data and places it into a new file
+	 * @param originalFile The file being filtered
+	 * @param filteredFile The file name where the filtered data will be stored
+	 * @return True if the data was filtered and saved, false if it wasn't
+	 */
+	public static boolean filterSite(String originalFile, String filteredFile) {
+		try {
+			Scanner reader = new Scanner(new File(originalFile));
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filteredFile)));
+			String line = reader.next();
+			
+			while (reader.hasNextLine()) {
+				if (line.contains("<p>")) {
+					line = filterLine(line);
+					line = trimLine(line);
+					line = cutEnding(line);
+					line = convertCharEntity(line);
+					writer.write(line);
+					writer.newLine();
+				}
+				
+				line = reader.nextLine();
+			}
+			
+			writer.close();
+			
+			return true;
+		} catch (Exception e) {
+			System.out.println(e);
+			System.exit(2);
+			return false;
+		}
+	}
+	
+	/**
+	 * Prints out an answer to a question if it is both understood and the computer has an answer
+	 * @param potential Files An ArrayList of locations where the requested info could be found
+	 * @param query The question that the user is asking
+	 */
+	public static void printQuery(ArrayList<String> potentialFiles, String query) {
+		try {
+			if (potentialFiles.isEmpty()) {
+				System.out.println("Sorry, I can not connect to the internet");
+				return;
+			}
+			
+			String[][] answers = new String[150][100];
+			int[] answerStrength = new int[150];
+			int stringCounter = 0;
+			int answerCounter = 0;
+			int fileCounter = 0;
+			int results = 0;
+			
+			while (potentialFiles.size() > fileCounter) {
+				String temp = potentialFiles.get(fileCounter);
+				answerStrength[results] = LockMatch.lock_match(temp, query);
+				
+				if (answerStrength[results] > 8) {
+					Scanner reader = new Scanner(new File("data/filtered/cdc-filtered-"+temp+".txt"));
+					
+					while (reader.hasNext()) {
+						answers[stringCounter][answerCounter] = reader.nextLine();
+						answerCounter++;
+					}
+					
+					results++;
+					stringCounter++;
+					answerCounter = 0;
+					reader.close();
+				}
+				
+				fileCounter++;
+			}
+			
+			if (results == 0) {
+				System.out.println("Sorry, I do not understand what you are saying");
+				return;
+			}
+			
+			int highestValue = 0;
+			int highestPos = 0;
+			
+			for (int i = 0; results > i; i++) {
+				if (answerStrength[i] > highestValue) {
+					highestValue = answerStrength[i];
+					highestPos = i;
+				}
+			}
+			
+			for (int i = 0; answers[highestPos].length > i; i++) {
+				if (answers[highestPos][i] == null) {
+					break;
+				}
+				
+				System.out.println(answers[highestPos][i]);
+			}
+			
+			return;
+		} catch (Exception e) {
+			System.out.println(e);
+			return;
+		}
+	}
+	
+	/**
+	 * Trims off all of the HTML tags and data that isn't a part of a <p> tag
+	 * @param input The String that will be trimmed
+	 * @return The trimmed String
+	 */
+	private static String filterLine(String input) {
+		StringBuilder line = new StringBuilder(input.trim());
+		int position = 0;
+		// trigger is used for activating a while loop that deletes every character before it gets to another p tag
+		
+		while (line.length() > position) {
+			
+			// Starts removing characters when it gets to the beginning of a tag
+			if (line.charAt(position) == '<') {
+				// Deletes the < and p character if they're there, if not then it sets trigger to true
+				if (line.charAt(position + 1) == 'p') {
+					line.deleteCharAt(position);
+					line.deleteCharAt(position);
+					line.deleteCharAt(position);
+				} else if (line.charAt(position + 1) == '/') {
+					if (line.charAt(position + 2) == 'p') {
+						line.deleteCharAt(position);
+						line.deleteCharAt(position);
+						line.deleteCharAt(position);
+						line.deleteCharAt(position);
+					}
+				}
+			}
+
+			// Ends the loop if the line is done being trimmed
+			if (position >= line.length()) {
+				break;
+			}
+			
+			// Moves position forward one if there isn't another tag to delete
+			if (line.charAt(position) != '<') {
+				position++;
+			} else if (line.charAt(position + 1) != 'p') {
+				position++;
+			}
+		}
+		
+		return line.toString();
+	}
+	
+	/**
+	 * Trims off the remaining tags in the string
+	 * @param input The string that is gonna be trimmed
+	 * @return A string that has no tags
+	 */
+	private static String trimLine(String input) {
+		StringBuilder line = new StringBuilder(input);
+		boolean secondTrigger = false;
+		boolean trigger = false;
+		int position = 0;
+		
+		while (line.length() > position) {
+			if (line.charAt(position) == '<') {
+				trigger = true;
+			}
+			
+			while (trigger) {
+				if (position + 1 > line.length()) { 
+					break;
+				}
+				
+				if (line.charAt(position) == '>') {
+					line.deleteCharAt(position);
+					
+					if (secondTrigger) {
+						trigger = false;
+						secondTrigger = false;
+						break;
+					} else {
+						secondTrigger = true;
+						
+						if (position + 1 >= line.length()) {
+							break;
+						}
+					}
+				} else {
+					line.deleteCharAt(position);
+				}
+			}
+			
+			if (position + 1 >= line.length()) {
+				break;
+			}
+			
+			if (line.charAt(position) != '<') {
+				position++;
+			}
+		}
+		
+		return line.toString();
+	}
+
+	/**
+	 * Cuts off the email portion of the website
+	 * @param input The line being potentially cut
+	 * @return A line that does not include the email ending of the CDC website
+	 */
+	private static String cutEnding(String input) {
+		if (input.equals("To receive email updates about this page, enter your email address:") || input.equals("We take your privacy seriously. You can review and change the way we collect information below.")) {
+			return "";
+		} else {
+			return input;
+		}
+	}
+	
+	/**
+	 * Converts character entities (Like &amp;) into normal characters
+	 * @param input The string that's being worked on
+	 * @return A string that has no character entities
+	 */
+	private static String convertCharEntity(String input) {
+		StringBuilder line = new StringBuilder(input);
+		int position = 0;
+		char type = 'a';
+		
+		while (line.length() > position) {
+			if (line.charAt(position) == '&') {
+				type = line.charAt(position + 1);
+				
+				while (line.charAt(position) != ';') {
+					line.deleteCharAt(position);
+				}
+				
+				line.deleteCharAt(position);
+				
+				if (type == 'n') {
+					line.replace(position, position, " ");
+				} else if (type == 'r') {
+					line.replace(position, position, "'");
+				}
+			}
+			
+			position++;
+		}
+		
+		return line.toString();
+	}
+	
+	/**
+	 * Filters out links from the provided String
+	 * @param input The String that may contain links
+	 * @return A string that either has links, or says "There was no links"
+	 */
+	private static String filterLink(String input) {
+		StringBuilder line = new StringBuilder(input);
+		StringBuilder output = new StringBuilder();
+		final int shift = 9;
+		int position = 0;
+		
+		while (line.length() > position) {
+			if (line.charAt(position) == '<') {
+				if (line.charAt(position + 1) == 'a') {
+					while (line.charAt(position + shift) != '"') {
+						output.append(line.charAt(position + shift));
+						position++;
+					}
+					
+					output.append(' ');
+					position = position + shift;
+				}
+			}
+			position++;
+		}
+		
+		if (output.toString().equals("")) {
+			return "There was no link";
+		}
+		
+		return output.toString();
+	}
+
+	/**
+	 * Organizes links from a filtered file and puts the organized links into a new file
+	 * @param file The file name of the file containing filtered, but not organized, links
+	 */
+	private static void organizeLinks(String file) {
+		try {
+			Scanner reader = new Scanner(new File("data/filtered/cdc-links-unorganized.txt"));
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+			String line = reader.next();
+
+			while (reader.hasNextLine()) {
+				if (line.length() > 5) {
+					if (line.contains(" ")) {
+						lineBreaker(line, writer);
+					}
+					
+					if (line.charAt(1) == 'f' && line.charAt(5) == 'a') {
+						if (50 > line.length()) {
+							writer.write(line);
+							writer.newLine();
+						}
+					}
+				}
+				
+				line = reader.nextLine();
+			}
+			
+			writer.close();
+			
+			return;
+		} catch (Exception e) {
+			System.out.println(e);
+			return;
+		}
+	}
+	
+	/**
+	 * Breaks up any lines with multiple links
+	 * @param input The line with multiple links
+	 * @param writer The writer which saves the links
+	 * @throws IOException In case an error saving occurs
+	 */
+	private static void lineBreaker(String input, BufferedWriter writer) throws IOException {
+		int startPos = 0;
+		int endPos = 0;
+		
+		while (input.length() > endPos) {
+			if (input.charAt(endPos) == ' ') {
+				if (input.charAt(1) == 'f' && input.charAt(5) == 'a') {
+					writer.write(input.substring(startPos, endPos));
+					writer.newLine();
+				}
+				
+				startPos = endPos + 1;
+			}
+			
+			endPos++;
+		}
+	}
+}
